1ea003b4cf5def1e8fff48662015aaf27c290559
unknown <adl774855@gmail.com>
2023-04-13 09:49:57 +0200

    secend

---
+++ chat.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { router } = require('./src/routes/users');
+app.use('/api/users', router)
+
+
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+
+io.on('connection', async (socket) => {
+
+  socket.on('chat-room', function (room) {
+    console.log(room)
+    socket.join(room);
+  });
+
+
+  socket.on('sendMsg' , (msg)=>{
+    console.log(msg)
+    io.to(msg.room).emit('message' , msg.msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:3000');
+});
+
+
+
+
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { router } = require('./src/routes/users');
+app.use('/api/users', router)
 
 
 const { Server } = require("socket.io");
+
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 
 io.on('connection', async (socket) => {
@@ -18,6 +21,19 @@ io.on('connection', async (socket) => {
     console.log(room)
     socket.join(room);
   });
+
+  // chat socket
+  // socket.on('chat-room', function (room) {
+  //   console.log(room)
+  //   socket.join(room);
+  // });
+
+
+  // live tracking
+  // socket.on('live-tracking', function (room) {
+  //   console.log(room)
+  //   socket.join(room);
+  // });
 
 
   socket.on('sendMsg' , (msg)=>{
@@ -36,7 +52,7 @@ io.on('connection', async (socket) => {
 
 });
 
-server.listen(3000, () => {
+server.listen(4000, () => {
   console.log('listening on *:3000');
 });
 
